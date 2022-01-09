4be426912cbfd33b1e3b8e58b7d32756ffd376d3
vritansh <vritansh14@gmail.com>
2022-01-09 14:45:32 +0530

    Components Props
    Pass data from parent to child
    Use props to pass complete component

--- components/src/CommentDetails.js
+++ components/src/CommentDetails.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const CommentDetail =(props) => {
-    console.log(props)
+   
 return (
    
     <div className="comment">
--- components/src/index.js
+++ components/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CommentDetail from './CommentDetails';
+import ApprovalCard from './ApprovalCard';
 
 function getTime() {
     return (new Date()).toLocaleTimeString()
@@ -10,21 +11,27 @@ function getTime() {
 const App = () => {
 
     return (
-
         <div className="ui container comments"> 
+        <ApprovalCard> 
+            <div> 
+                    Are you sure you want to do this
+             </div>
+             </ApprovalCard>
+          
+            <ApprovalCard>
            <CommentDetail author="Sammy"
-                time={getTime()}
-                
+                time={getTime()}   
            />
+           </ApprovalCard>
+           <ApprovalCard>
            <CommentDetail author ="Alex "
                 time={getTime()}
            />
+           </ApprovalCard>
            <CommentDetail author ="Jane"
                 time={getTime()}
            />
         </div>
-       
-
     );
 };
 
