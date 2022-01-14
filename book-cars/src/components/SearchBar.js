9a8e63d631c2d23e0453a15851c27089d9e604be
vritansh <vritansh14@gmail.com>
2022-01-15 00:14:34 +0530

    added arrow functions

--- book-cars/src/components/SearchBar.js
+++ book-cars/src/components/SearchBar.js
@@ -2,11 +2,19 @@ import React from 'react';
 
 class SearchBar extends React.Component
 {
+state = { term: ''};
+
+onFormSubmit = e =>
+{
+e.preventDefault();
+console.log(this.state.term)
+}
+
 state = { term : 'Hi Tehre'}
     render() {
    return (
      <div className="ui segment"> 
-        <form className="ui form"> 
+        <form onSubmit={this.onFormSubmit} className="ui form"> 
             <div>
                 <label>Image search </label>
                 <input value={this.state.term} onChange={ (event) => this.setState({term: event.target.value})} type="text"/>
