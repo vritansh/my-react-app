813839e103e9b086fbbd3d42ec994dee1b6602b5
vritansh <vritansh14@gmail.com>
2022-01-15 23:28:47 +0530

    Propagate event from parent to child via Card

--- book-cars/src/components/ImageList.js
+++ book-cars/src/components/ImageList.js
@@ -1,10 +1,12 @@
 import React from "react";
 import './ImageList.css';
+import ImageCard from './ImageCard';
 
 const ImageList = (props)=> {
-   const images =  props.images.map( (image) => {
-        return <img src={image.urls.regular} key ={image.id}/>
+   const images =  props.images.map(image => {
+        return <ImageCard key={image.id} image={image} />
     });
+
     return <div className="image-list"> {images}</div>
 }
 
