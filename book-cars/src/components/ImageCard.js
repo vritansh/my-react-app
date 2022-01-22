5f43f872f7340eb7179bfffe289d988a21ef1002
vritansh <vritansh14@gmail.com>
2022-01-22 15:32:24 +0530

    Final commit for test application
    complete app

--- book-cars/src/components/ImageCard.js
+++ book-cars/src/components/ImageCard.js
@@ -1,10 +1,29 @@
 import React from 'react';
 
 class ImageCard extends React.Component{
+    constructor(props)
+    {
+
+        super(props);
+        this.state = {spans:0}; 
+        this.imageRef = React.createRef();
+
+    }
+    componentDidMount(){
+        this.imageRef.current.addEventListener('load', this.setSpans);
+    }
+    setSpans= ()=>  {
+        const height = this.imageRef.current.clientHeight;
+        const spans = Math.ceil(height/10) ;
+
+        this.setState({spans})
+
+    }
     render() {
+        const {description, urls} = this.props.image; 
         return (
-            <div> 
-                <img 
+            <div style={{ gridRowEnd:`span ${this.state.spans}` }}> 
+                <img ref={this.imageRef}
                     alt={this.props.image.description}
                     src ={this.props.image.urls.regular}
                 />
@@ -12,6 +31,8 @@ class ImageCard extends React.Component{
             </div>
         )
     }
+
+
 }
 
 export default ImageCard;
