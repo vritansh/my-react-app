95aef2bc925b7fe44a670f590788baa6e764cbca
vritansh <vritansh14@gmail.com>
2022-01-27 01:47:50 +0530

    wikipedia search

--- widgets/src/components/Search.js
+++ widgets/src/components/Search.js
@@ -1,8 +1,33 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import axios from 'axios';
 
 const Search = () => 
 {
-    const [ term, setTerm] = useState('');
+    const [ term, setTerm] = useState('programming');
+    const [results, setResults] = useState([]); 
+    console.log(results);
+
+    useEffect(()=>
+    {
+       const search = async () => {
+        const {data}  =  await  axios.get('https://en.wikipedia.org//w/api.php',{
+            params: {
+                action: 'query',
+                list : 'search',
+                origin :'*',
+                format: 'json',
+                srsearch: term,
+            },
+        })
+
+        setResults(data);
+       
+    };
+    
+    }, [term]);
+  
+    // const renderedResults = results.
+
      return (
 <div> 
     <div className="ui form"> 
